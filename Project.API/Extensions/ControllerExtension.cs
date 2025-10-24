dd237fbe6273bb72f3af58be443b82171029494b
Ngo Xuan Hai <hngoxuantg.dev@gmail.com>
2025-10-24 14:57:57 +0700

    Refactor AddCustomControllers for clarity and cycle handling
    Refactored the `AddCustomControllers` method to consolidate
    `AddControllers` calls, improving readability and reducing
    redundancy. Added `ReferenceHandler.IgnoreCycles` to prevent
    circular reference issues during JSON serialization. Retained
    existing JSON options and improved code structure for better
    maintainability.

--- Project.API/Extensions/ControllerExtension.cs
+++ Project.API/Extensions/ControllerExtension.cs
@@ -9,19 +9,18 @@ namespace Project.API.Extensions
     {
         public static IServiceCollection AddCustomControllers(this IServiceCollection services)
         {
-            services.AddControllers()
-                .AddJsonOptions(options =>
-                {
-                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-                    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-                    options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
-                    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-                });
-
             services.AddControllers(options =>
             {
                 options.Conventions.Add(
                     new RouteTokenTransformerConvention(new LowercaseRouteParameterTransformer()));
+            })
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
             });
 
             return services;
